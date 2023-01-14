4c0e66236dc957387230046b8efaf5844b907556
alexisns <alexisns@pm.me>
2023-01-14 19:32:43 +0100

    premier

---
+++ app/javascript/application.js
@@ -0,0 +1,144 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+
+
+/* 
+    pointer.js was created by OwL for use on websites, 
+     and can be found at https://seattleowl.com/pointer.
+*/
+
+const pointer = document.createElement("div")
+pointer.id = "pointer-dot"
+const ring = document.createElement("div")
+ring.id = "pointer-ring"
+document.body.insertBefore(pointer, document.body.children[0])
+document.body.insertBefore(ring, document.body.children[0])
+
+let mouseX = -100
+let mouseY = -100
+let ringX = -100
+let ringY = -100
+let isHover = false
+let mouseDown = false
+const init_pointer = (options) => {
+
+    window.onmousemove = (mouse) => {
+        mouseX = mouse.clientX
+        mouseY = mouse.clientY
+    }
+
+    window.onmousedown = (mouse) => {
+        mouseDown = true
+    }
+
+    window.onmouseup = (mouse) => {
+        mouseDown = false
+    }
+
+    const trace = (a, b, n) => {
+        return (1 - n) * a + n * b;
+    }
+    window["trace"] = trace
+
+    const getOption = (option) => {
+        let defaultObj = {
+            pointerColor: "#750c7e",
+            ringSize: 15,
+            ringClickSize: (options["ringSize"] || 15) - 5,
+        }
+        if (options[option] == undefined) {
+            return defaultObj[option]
+        } else {
+            return options[option]
+        }
+    }
+
+    const render = () => {
+        ringX = trace(ringX, mouseX, 0.2)
+        ringY = trace(ringY, mouseY, 0.2)
+
+        if (document.querySelector(".p-action-click:hover")) {
+            pointer.style.borderColor = getOption("pointerColor")
+            isHover = true
+        } else {
+            pointer.style.borderColor = "white"
+            isHover = false
+        }
+        ring.style.borderColor = getOption("pointerColor")
+        if (mouseDown) {
+            ring.style.padding = getOption("ringClickSize") + "px"
+        } else {
+            ring.style.padding = getOption("ringSize") + "px"
+        }
+
+        pointer.style.transform = `translate(${mouseX}px, ${mouseY}px)`
+        ring.style.transform = `translate(${ringX - (mouseDown ? getOption("ringClickSize") : getOption("ringSize"))}px, ${ringY - (mouseDown ? getOption("ringClickSize") : getOption("ringSize"))}px)`
+
+        requestAnimationFrame(render)
+    }
+    requestAnimationFrame(render)
+}
+
+
+
+
+
+
+
+
+
+(function(){
+
+    var doc = document.documentElement;
+    var w = window;
+  
+    var prevScroll = w.scrollY || doc.scrollTop;
+    var curScroll;
+    var direction = 0;
+    var prevDirection = 0;
+  
+    var header = document.getElementById('site-header');
+  
+    var checkScroll = function() {
+  
+      /*
+      ** Find the direction of scroll
+      ** 0 - initial, 1 - up, 2 - down
+      */
+  
+      curScroll = w.scrollY || doc.scrollTop;
+      if (curScroll > prevScroll) { 
+        //scrolled up
+        direction = 2;
+      }
+      else if (curScroll < prevScroll) { 
+        //scrolled down
+        direction = 1;
+      }
+  
+      if (direction !== prevDirection) {
+        toggleHeader(direction, curScroll);
+      }
+      
+      prevScroll = curScroll;
+    };
+  
+    var toggleHeader = function(direction, curScroll) {
+      if (direction === 2 && curScroll > 52) { 
+        
+        //replace 52 with the height of your header in px
+  
+        header.classList.add('hide');
+        prevDirection = direction;
+      }
+      else if (direction === 1) {
+        header.classList.remove('hide');
+        prevDirection = direction;
+      }
+    };
+    
+    window.addEventListener('scroll', checkScroll);
+  
+  })();
