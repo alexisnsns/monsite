2648417c6e32cd96626914f4810de7268316f1ff
alexisns <alexisns@pm.me>
2023-01-17 18:47:01 +0100

    curseur

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,90 +4,6 @@ import "controllers"
 
 
 
-/* 
-    pointer.js was created by OwL for use on websites, 
-     and can be found at https://seattleowl.com/pointer.
-*/
-
-const pointer = document.createElement("div")
-pointer.id = "pointer-dot"
-const ring = document.createElement("div")
-ring.id = "pointer-ring"
-document.body.insertBefore(pointer, document.body.children[0])
-document.body.insertBefore(ring, document.body.children[0])
-
-let mouseX = -100
-let mouseY = -100
-let ringX = -100
-let ringY = -100
-let isHover = false
-let mouseDown = false
-const init_pointer = (options) => {
-
-    window.onmousemove = (mouse) => {
-        mouseX = mouse.clientX
-        mouseY = mouse.clientY
-    }
-
-    window.onmousedown = (mouse) => {
-        mouseDown = true
-    }
-
-    window.onmouseup = (mouse) => {
-        mouseDown = false
-    }
-
-    const trace = (a, b, n) => {
-        return (1 - n) * a + n * b;
-    }
-    window["trace"] = trace
-
-    const getOption = (option) => {
-        let defaultObj = {
-            pointerColor: "#750c7e",
-            ringSize: 15,
-            ringClickSize: (options["ringSize"] || 15) - 5,
-        }
-        if (options[option] == undefined) {
-            return defaultObj[option]
-        } else {
-            return options[option]
-        }
-    }
-
-    const render = () => {
-        ringX = trace(ringX, mouseX, 0.2)
-        ringY = trace(ringY, mouseY, 0.2)
-
-        if (document.querySelector(".p-action-click:hover")) {
-            pointer.style.borderColor = getOption("pointerColor")
-            isHover = true
-        } else {
-            pointer.style.borderColor = "white"
-            isHover = false
-        }
-        ring.style.borderColor = getOption("pointerColor")
-        if (mouseDown) {
-            ring.style.padding = getOption("ringClickSize") + "px"
-        } else {
-            ring.style.padding = getOption("ringSize") + "px"
-        }
-
-        pointer.style.transform = `translate(${mouseX}px, ${mouseY}px)`
-        ring.style.transform = `translate(${ringX - (mouseDown ? getOption("ringClickSize") : getOption("ringSize"))}px, ${ringY - (mouseDown ? getOption("ringClickSize") : getOption("ringSize"))}px)`
-
-        requestAnimationFrame(render)
-    }
-    requestAnimationFrame(render)
-}
-
-
-
-
-
-
-
-
 
 (function(){
 
@@ -142,3 +58,42 @@ const init_pointer = (options) => {
     window.addEventListener('scroll', checkScroll);
   
   })();
+
+
+
+
+var cursor = document.querySelector('.cursor');
+var cursorinner = document.querySelector('.cursor2');
+var a = document.querySelectorAll('a');
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursorinner.style.left = x + 'px';
+  cursorinner.style.top = y + 'px';
+});
+
+document.addEventListener('mousedown', function(){
+  cursor.classList.add('click');
+  cursorinner.classList.add('cursorinnerhover')
+});
+
+document.addEventListener('mouseup', function(){
+  cursor.classList.remove('click')
+  cursorinner.classList.remove('cursorinnerhover')
+});
+
+a.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('hover');
+  });
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('hover');
+  });
+})
